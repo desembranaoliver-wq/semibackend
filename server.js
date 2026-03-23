7b7335ad9aa5012d3cb487d8f2388be6c3e09a51
desembranaoliver-wq <desembranaoliver@gmail.com>
2026-03-23 11:42:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./config/db');         // db.js must be inside /config/
-const routes = require('./routes/index'); // routes file is /routes/index.js
-
+const db = require('./config/db');
+const routes = require('./routes/index');
+ 
 const app = express();
-
+ 
 const moment = require('moment');
 const logger = (req, res, next) => {
     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} - ${moment().format()}`);
     next();
 };
-
+ 
 app.use(logger);
-app.use(cors());
+app.use(cors({
+    origin: 'https://semifrontend-kappa.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+ 
 app.use('/api', routes);
-
+ 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
